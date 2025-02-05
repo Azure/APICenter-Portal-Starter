b5d3bee8623769cc90ca84c7511a3b5cdd197e12
eugenetsakh <onatolich@gmail.com>
2025-02-05 18:06:50 +0100

    upd

--- src/atoms/appServicesAtom.ts
+++ src/atoms/appServicesAtom.ts
@@ -5,6 +5,7 @@ import { IAuthService } from '@/types/services/IAuthService';
 import ApiService from '@/services/ApiService';
 import MsalAuthService from '@/services/MsalAuthService';
 
+// If adding more services here - make sure to restrict their default implementations imports in eslint config (no-restricted-imports rule)
 export interface AppServicesAtomState {
   ApiService?: IApiService;
   AuthService?: IAuthService;
