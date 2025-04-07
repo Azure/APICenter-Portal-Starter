78fca276dc1b139c9af4781f018d6044e655c407
Alexander Zaslonov <alzaslon@microsoft.com>
2025-04-07 15:50:06 -0700

    Small fixes for semantic search support.

--- src/experiences/ApiList/apiAdapter.ts
+++ src/experiences/ApiList/apiAdapter.ts
@@ -1,13 +1,15 @@
 import { Api as DocsApi } from '@microsoft/api-docs-ui';
 import { ApiMetadata } from '@/types/api';
 
-export default function apiAdapter(api: any): DocsApi {
+export default function apiAdapter(api: any): DocsApi & ApiMetadata {
   const summary = api.summary;
 
   return {
     name: api.name,
+    title: api.title || api.api,
     displayName: api.title || api.api,
     description: summary,
     type: api.kind,
+    lifecycleStage: api.lifecycleStage,
   };
 }
