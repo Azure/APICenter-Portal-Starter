68e63edabdcd493c459395b5ede05011758c5e0c
Alexander Zaslonov <alzaslon@microsoft.com>
2025-04-10 10:42:58 -0700

    Added check for 'mcp' type to hide it for time being.

--- src/pages/ApiSpec/ApiSpecPageLayout/ApiSpecPageLayout.tsx
+++ src/pages/ApiSpec/ApiSpecPageLayout/ApiSpecPageLayout.tsx
@@ -19,7 +19,7 @@ export const ApiSpecPageLayout: React.FC<Props> = ({ definitionId, deployment, a
 
   if (!apiSpec?.type) {
     return (
-      <EmptyStateMessage>The specified API does not exist or its specification can&apos;t be read</EmptyStateMessage>
+      <EmptyStateMessage>The specified API does not exist or its documentation can&apos;t be displayed.</EmptyStateMessage>
     );
   }
 
